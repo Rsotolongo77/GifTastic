a9d84093146040eebc8fedfce294056b41b9a20f
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-09-14 16:03:10 -0400

    added API key and url to javascript.

---
+++ Assets/javascript/app.js
@@ -0,0 +1,3 @@
+var APIKey = "hh9ypFLslCSgV8p8wP7IQnLgbwS0Kp88";
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=hh9ypFLslCSgV8p8wP7IQnLgbwS0Kp88&q=&limit=25&offset=0&rating=G&lang=en";
