e6dd9728008f200f8ea4d4c52391054ed8c13044
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-09-15 16:03:20 -0400

    render button function built.

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,93 +1,29 @@
-//var APIKey = "hh9ypFLslCSgV8p8wP7IQnLgbwS0Kp88";
+var buttons = ["The Office", "Dogs", "90s", "Parrots", "Awkward", "Drunk", "Computer", "Toy Story", "Laughing", "Happy Dance"];
 
-//var topics = ["The Office", "Dogs", "90s", "Parrots", "Awkward", "Drunk", "Computer", "Toy Story", "Laughing", "Happy Dance"];
+function renderButtons() {
 
-
-//function displayGifInfo() {
-
-$("button").on("click", function(){
-
-    var topic = $(this).attr("data-topic");
-
-
-
-   
-
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=hh9ypFLslCSgV8p8wP7IQnLgbwS0Kp88&limit=5";
     
 
+    for (i = 0; i < buttons.length; i++) {
+        var buttonIndex = buttons[i];
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
+        var button = ` <div class="container-buttons">
+        <button 
+            class="btn btn-search"
+            data-name="${buttonIndex}">${buttonIndex}</button>
+        
+            <button data-name="${buttonIndex}"
+            class="btn btn-delete fas fa-times"
+            ></button>
+        
+        </div>
+        
+        `;
 
-
-
-    console.log(queryURL);
-    console.log(response);
-
-    var results = response.data;
-    for (var i =0; i < results.length; i++) {
-
-    var gifDiv = $("<div>");
-    var p = $("<p>").text("Rating: " + results[i].rating);
-    var gifImage = $("<img>");
-    gifImage.attr("src", results[i].images.fixed_height.url);
-    gifDiv.append(p);
-    gifDiv.append(gifImage);
-    $("#gifs-view").prepend(gifDiv);        
+        $(".recents").append(button);
     }
 
-});
+}
 
-});   
-    //var gifDiv = $("<div class='gifs'>");
-    //var Rating = response.rating;
-    //var ratingDisplay = $("<p>").text("Rating: " + Rating);
-    //gifDiv.append(ratingDisplay);
-    //var imgUrl = response.data.images;
-    //var img = $("<img>").attr("src",imgUrl);
-    //gifDiv.append(img);
-    
+renderButtons();
 
-//console.log(displayGifInfo);
-
-
-
-
-
-//function renderButtons () {
-
-    //$("#buttons-view").empty();
-    //for (var i = 0; i < topics.length; i++) {
-
-        //var buttons = $("<button>");
-        //buttons.addClass("gif-btn");
-        //buttons.attr("data-name", topics[i]);
-        //buttons.text(topics[i]);
-        //$("#buttons-view").append(buttons);
-      
-
-
-
-//renderButtons();
-
-
-//$("#buttons-view").on("click", function(event) {
-    //event.preventDefault();
- 
-    //var gifs = $().val().trim();
-
-    // Adding movie from the textbox to our array
-    //movies.push(movie);
-
-    // Calling renderButtons which handles the processing of our movie array
-    //renderButtons();
-  //});
-
-  // Adding a click event listener to all elements with a class of "movie-btn"
-  //$(document).on("click", ".movie-btn", displayMovieInfo);
-
-  // Calling the renderButtons function to display the intial buttons
-  //renderButtons();
