6e44767a8ba6e2413eb07eb3e27d327c09222cd0
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-09-16 17:31:39 -0400

    added search bar and addto current gifs  button.

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -56,6 +56,19 @@ function renderButtons() {
 
 }
 
+$(document).on("click", ".gifs-created", function() {
+    
+    var state = $(this).attr("data-state");
+   
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
 
 $(document).on("click", ".gif-button", displayGifInfo)
 
