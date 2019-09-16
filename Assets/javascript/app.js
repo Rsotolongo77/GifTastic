68f91eedabb8d0587b522be276d91d751c35dbf6
Ricardo Sotolongo <rickey.soto@gmail.com>
2019-09-16 12:13:05 -0400

    added functionality to get gifs to display with ajax call.

--- Assets/javascript/app.js
+++ Assets/javascript/app.js
@@ -1,50 +1,65 @@
 var buttons = ["The Office", "Dogs", "90s", "Parrots", "Awkward", "Drunk", "Computer", "Toy Story", "Laughing", "Happy Dance"];
 
-function loadButtons()  {
-    var listButtons = JSON.parse(localStorage.getItem("buttons"));
-    buttons = listButtons;
+
+function displayGifInfo() {
+
+    var gif = $(this).attr("data-name");
+
+  
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=hh9ypFLslCSgV8p8wP7IQnLgbwS0Kp88&limit=10";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+
+        var results = response.data;
+        for (var i = 0; i < results.length; i++) {
+
+
+            var gifDiv = $(`<div class="gifs-created">`);
+            var p = $("<p>").text("Rating: " + results[i].rating);
+            var gifImg = $("<img>");
+            gifImg.attr("src", results[i].images.fixed_height.url);
+            gifDiv.append(p);
+            gifDiv.append(gifImg);
+            $("#gifs-view").prepend(gifDiv);
+        }
+
+    })
+
+    
 }
+
+
 
 function renderButtons() {
 
-    $(".recents").empty();
 
-    for (i = 0; i < buttons.length; i++) {
-        var buttonIndex = buttons[i];
+    $("#buttons-view").empty();
 
-        var button = ` <div class="container-buttons">
-        <button 
-            class="btn btn-search"
-            data-name="${buttonIndex}">${buttonIndex}</button>
-        
-            <button data-name="${buttonIndex}"
-            class="btn btn-delete fas fa-times"
-            ></button>
-        
-        </div>
-        
-        `;
 
-        $(".recents").append(button);
+    for (var i = 0; i < buttons.length; i++) {
 
-        localStorage.setItem("buttons", JSON.stringify(buttons));
+
+        var a = $("<button>");
+        a.addClass("gif-button");
+        a.attr("data-name", buttons[i]);
+        a.text(buttons[i]);
+        $("#buttons-view").append(a);
+
+
+
     }
 
 }
 
-loadButtons();
+
+$(document).on("click", ".gif-button", displayGifInfo)
+
+
+
+
 renderButtons();
-
-
-
-$("#submit-btn").on("click ", function(event)  {
-
-event.preventDefault(); 
- var value = $("#search").val();
-
- buttons.push(value);
- renderButtons();
-
- console.log ("Value", value);
-});
-
